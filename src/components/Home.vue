4faa93a491e8ba452e5e5c5f5903a451b41fc423
Lourengato <lourengato@gmail.com>
2021-07-18 12:57:51 -0300

    today cards

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,9 +10,59 @@
         </template>
         <template #footer>
           <div class="flex">
-            <div class="text--white app__card-label">{{ formatTemperature(item.min_temp) }}º</div>
-            <div class="text--gray">{{ formatTemperature(item.max_temp) }}º</div>
+            <div class="text--white app__card-label">{{ formatRound(item.min_temp) }}º</div>
+            <div class="text--gray">{{ formatRound(item.max_temp) }}º</div>
           </div>
+        </template>
+      </card>
+    </div>
+    <div class="flex">
+      <card color="#1E213A" class="flex flex--align-center flex--column app__card">
+        <template #header>
+          <div class="text--white">Wind status</div>
+        </template>
+        <template #body>
+          <div class="text--white">{{ formatRound(weather[0].wind_speed) }} mph</div>
+        </template>
+        <template #footer>
+          <div class="flex">
+            <div class="text--white">
+              <navigation-icon />
+              wsw
+            </div>
+          </div>
+        </template>
+      </card>
+
+      <card color="#1E213A" class="flex flex--align-center flex--column app__card">
+        <template #header>
+          <div class="text--white">Humidity</div>
+        </template>
+        <template #body>
+          <div class="text--white">{{ formatRound(weather[0].humidity) }}%</div>
+        </template>
+        <template #footer>
+          <div class="flex">
+            <div class="text--white">Wind status</div>
+          </div>
+        </template>
+      </card>
+
+      <card color="#1E213A" class="flex flex--align-center flex--column app__card">
+        <template #header>
+          <div class="text--white">Visibility</div>
+        </template>
+        <template #body>
+          <div class="text--white">{{ formatRound(weather[0].visibility) }} miles</div>
+        </template>
+      </card>
+
+      <card color="#1E213A" class="flex flex--align-center flex--column app__card">
+        <template #header>
+          <div class="text--white">Air Pressure</div>
+        </template>
+        <template #body>
+          <div class="text--white">{{ formatRound(weather[0].air_pressure) }} mb</div>
         </template>
       </card>
     </div>
@@ -39,13 +89,15 @@ import Btn from './Btn.vue'
 import CitySelect from './CitySelect.vue'
 import SearchCity from './SearchCity.vue'
 import { mapActions, mapGetters } from 'vuex'
+import NavigationIcon from 'vue-material-design-icons/Navigation.vue';
 
 export default {
   components: {
     Card,
     Btn,
     CitySelect,
-    SearchCity
+    SearchCity,
+    NavigationIcon
   },
 
   data () {
@@ -70,14 +122,16 @@ export default {
       const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
       var data = new Date(date)
       var currentDate = new Date()
+
       if (data.getDate() + 1 === currentDate.getDate() || data.getDate() === currentDate.getDate()) {
         return data.getDate() === currentDate.getDate() ? 'Today' : 'Tomorrow'
       }
+
       let dataFormatada = ((data.getDate() + 1 )) + ", " + weeks[data.getUTCDay()] + " " + months[(data.getMonth())]
       return dataFormatada
     },
 
-    formatTemperature (Temperature) {
+    formatRound (Temperature) {
       return Math.round(Temperature)
     }
   }
